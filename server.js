69fe5b128e1e67d8936c30251a27b2494575ea73
Pankaj Gotefode <pankajgotefode123@gmail.com>
2021-07-25 14:07:38 +0530

    added route

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require("express");
+let app = express();
+
+app.get("/", (req, res) => {
+  res.send("Kaushik");
+});
+
+app.get("/api/login", (req, res) => {
+  console.log(req);
+  const responce = { success: true };
+  res.send(responce);
+});
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`your server is running on port ${port}`);
+});
