cfae9f01a0a983677ba62d4eefbb765edb631e1b
Pankaj Gotefode <pankajgotefode123@gmail.com>
2021-07-26 20:16:19 +0530

    routes created

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin:admin@cluster0.mhj6u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 let app = express();
 
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
-  res.send("Kaushik");
+  res.send("hello pankaj");
 });
 
-app.get("/api/login", (req, res) => {
-  console.log(req);
-  const responce = { success: true };
-  res.send(responce);
-});
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`your server is running on port ${port}`);
-});
+app.listen(port, () => console.log(`your server is running on port ${port}`));
